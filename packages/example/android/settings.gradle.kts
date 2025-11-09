030e5b90c632ee0647699e795d23f1d2eb7d855f
lhlyu <misakafs@gmail.com>
2025-11-10 01:07:19 +0800

    fix: gradle config

--- packages/example/android/settings.gradle.kts
+++ packages/example/android/settings.gradle.kts
@@ -10,28 +10,9 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        maven { url=uri ("https://www.jitpack.io")}
-        maven { url=uri ("https://maven.aliyun.com/repository/releases")}
-        maven { url=uri ("https://maven.aliyun.com/repository/google")}
-        maven { url=uri ("https://maven.aliyun.com/repository/central")}
-        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin")}
-        maven { url=uri ("https://maven.aliyun.com/repository/public")}
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        maven { url=uri ("https://www.jitpack.io")}
-        maven { url=uri ("https://maven.aliyun.com/repository/releases")}
-        maven { url=uri ("https://maven.aliyun.com/repository/google")}
-        maven { url=uri ("https://maven.aliyun.com/repository/central")}
-        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin")}
-        maven { url=uri ("https://maven.aliyun.com/repository/public")}
-        google()
-        mavenCentral()
     }
 }
 
